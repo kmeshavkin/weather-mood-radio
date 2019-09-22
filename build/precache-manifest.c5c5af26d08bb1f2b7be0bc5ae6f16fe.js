3e0f66b67e083163c9c5dd0d3cdb52129c68fbc5
Kostyan1996 <meshavkin1996@gmail.com>
2019-09-22 20:14:20 +0300

    Added build

---
+++ build/precache-manifest.c5c5af26d08bb1f2b7be0bc5ae6f16fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7007f7b7380c9f0ebc9b9d906b77841",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01e7a8a431ff799c42b4",
+    "url": "/static/css/main.741aa1b0.chunk.css"
+  },
+  {
+    "revision": "dd8805309951e5717057",
+    "url": "/static/js/2.cafb1267.chunk.js"
+  },
+  {
+    "revision": "01e7a8a431ff799c42b4",
+    "url": "/static/js/main.d70161a0.chunk.js"
+  },
+  {
+    "revision": "68ecb5ce3c3ad275038c",
+    "url": "/static/js/runtime~main.596befa4.js"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/static/media/react.05a3e30f.svg"
+  },
+  {
+    "revision": "4d2e3260c1637df6bc8cdc19c42fc6c3",
+    "url": "/static/media/record.4d2e3260.svg"
+  },
+  {
+    "revision": "5ae1af16ffc2f55bd739da81176302e9",
+    "url": "/static/media/redux.5ae1af16.svg"
+  }
+]);
